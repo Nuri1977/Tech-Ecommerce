5cbdae5c7760be96c352d989b05a1e0f3efaa645
Nuri1977 <nurilacka@gmail.com>
2023-01-06 09:43:11 +0100

    Add react router and Registration component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
 import './default.scss';
 import Homepage from './pages/Homepage/Homepage';
+import Registration from './pages/Registration/Registration';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className="main">
-        <Homepage />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/registration" element={<Registration />} />
+        </Routes>
       </main>
     </div>
   );
