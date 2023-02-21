4d23b671bb2678376c4fc17997fadda95fd37cbf
nurilacka <nurilacka@gmail.com>
2023-02-21 10:05:21 +0100

    Edit Navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Header.scss';
-import Logo from '../../assets/images/logo.png';
-import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../../assets/images/tm_new_logo_new.png';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { signOutFun } from '../../redux/authentication/authThunk';
 import { useAppDispatch, useAppSelector } from '../../redux/app/hooks';
 import useAuth from '../../hooks/useAuth';
@@ -18,70 +18,82 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="wrap">
-        <div className="logo" onClick={() => navigate('/')}>
-          <Link to="/">
-            <img src={Logo} alt="SimpleTut Logo" />
-          </Link>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/search">Search</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="callToActions">
-          <ul>
-            {!currentUser && (
-              <>
-                <li>
-                  <Link to="/registration">Registration</Link>
-                </li>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-              </>
-            )}
-            {currentUser && isAdmin && (
-              <li>
-                <Link to="/admin">Admin</Link>
-              </li>
-            )}
-            {currentUser && !isAdmin && (
-              <li>
-                <Link to="/dashboard">Dashboard</Link>
-              </li>
-            )}
-            {currentUser && (
-              <li>
-                <span onClick={() => dispatch(signOutFun())}>Logout</span>
-              </li>
-            )}
-            <li>
-              <div
-                className="cart"
-                onMouseEnter={() => setShowModal(true)}
-                onMouseLeave={() => setShowModal(false)}>
-                <BsCart3 size="24px" className="cartIcon" />
-                {cartartItemsCount > 0 && (
-                  <div className="cartQuantity">
-                    <div className="items">{cartartItemsCount}</div>
-                  </div>
-                )}
-                {showModal && (
-                  <div className="cartModal">
-                    <CartModal />
-                  </div>
-                )}
-              </div>
-            </li>
-          </ul>
-        </div>
+      <div className="logo" onClick={() => navigate('/')}>
+        <Link to="/">
+          <img src={Logo} alt="SimpleTut Logo" />
+        </Link>
       </div>
+      <nav>
+        <ul className="navList">
+          <li>
+            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/search" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+              E-shop
+            </NavLink>
+          </li>
+          {!currentUser && (
+            <>
+              <li>
+                <NavLink
+                  to="/registration"
+                  className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+                  Registration
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+                  Login
+                </NavLink>
+              </li>
+            </>
+          )}
+          {currentUser && isAdmin && (
+            <li>
+              <NavLink to="/admin" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+                Admin
+              </NavLink>
+            </li>
+          )}
+          {currentUser && !isAdmin && (
+            <li>
+              <NavLink
+                to="/dashboard"
+                className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+                Dashboard
+              </NavLink>
+            </li>
+          )}
+          {currentUser && (
+            <li>
+              <span onClick={() => dispatch(signOutFun())}>Logout</span>
+            </li>
+          )}
+          <li>
+            <div
+              className="cart"
+              onMouseEnter={() => setShowModal(true)}
+              onMouseLeave={() => setShowModal(false)}>
+              <BsCart3 size="24px" className="cartIcon" />
+              {cartartItemsCount > 0 && (
+                <div className="cartQuantity">
+                  <div className="items">{cartartItemsCount}</div>
+                </div>
+              )}
+              {showModal && (
+                <div className="cartModal">
+                  <CartModal />
+                </div>
+              )}
+            </div>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
