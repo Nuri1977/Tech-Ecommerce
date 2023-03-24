2f19dd05cb7a9668a624f8c64668c9b26eab5760
nurilacka <nurilacka@gmail.com>
2023-03-24 15:49:28 +0100

    Footer unit test

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,10 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="wrap">&copy; Simpletut 2020</div>
+    <div className="footer" data-testid="footer">
+      <div className="wrap" data-testid="wrap">
+        &copy; Simpletut 2020
+      </div>
     </div>
   );
 };
